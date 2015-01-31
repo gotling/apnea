69b5d8014c24fa2a2abd7919751d744d69546bac
Marcus Gotling <marcus@gotling.se>
2015-01-31 13:34:50 +0100

    Display correct data in chart

--- src/apps/dive_log/views.py
+++ src/apps/dive_log/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Session, Dive
+from .models import Session, Dive, DataPoint
 
 
 def list_sessions(request):
@@ -14,9 +14,15 @@ def list_sessions(request):
 def details(request, dive_id):
     dive = Dive.objects.get(id=dive_id)
 
+    data_points = DataPoint.objects.filter(dive_id=dive_id).values_list('second', 'depth', 'heart_rate')
+
+    time, depth, heart_rate = zip(*data_points)
+
     context = {
-        'dive': dive
+        'dive': dive,
+        'time': [t for t in time],
+        'depth': [float(d) for d in depth],
+        'heart_rate': [hr for hr in heart_rate]
     }
 
     return render(request, 'chart2.html', context)
-    #return render(request, 'details.html', context)
