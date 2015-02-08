59ba607cf7f04a97c65ec1af2b4bab9cdd11383e
Marcus Gotling <marcus@gotling.se>
2015-02-08 22:15:44 +0100

    Settings for site, used in account pages

--- src/apps/pinax_theme_bootstrap/conf.py
+++ src/apps/pinax_theme_bootstrap/conf.py
@@ -6,7 +6,8 @@ from appconf import AppConf
 class ThemeAppConf(AppConf):
 
     ADMIN_URL = "admin:index"
-    CONTACT_EMAIL = "support@example.com"
+    CONTACT_EMAIL = "support@apnea.gosy.se"
+    SITE_NAME = "Apnea"
 
     class Meta:
         prefix = "theme"
